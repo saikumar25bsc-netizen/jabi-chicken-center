604e4368d23db26efd3a91c523164601ea6056cb
saikumar25bsc-netizen <saikumar25bsc@ifheindia.org>
2026-06-06 22:53:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function orderViaWhatsApp(itemName) {
-    // ఇక్కడ 91 పక్కన మీ మొబైల్ నెంబర్ ఇవ్వండి
-    var phoneNumber = "919999999999"; 
+    // మీ కరెక్ట్ మొబైల్ నెంబర్ (ముందు 91 ఇండియా కోడ్ ఉండాలి)
+    var phoneNumber = "9100587831"; 
     
-    var message = "Hello Jabi Chicken Center, I want to order: " + itemName;
+    // కస్టమర్‌కు మరియు మీకు అర్థమయ్యేలా వెళ్ళే ఆర్డర్ మెసేజ్
+    var message = "Hello Jabi Chicken Center, I want to order " + itemName + ". Please confirm my order.";
+    
+    // వాట్సాప్ లింక్ క్రియేషన్
     var whatsappURL = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
     
+    // కొత్త ట్యాబ్‌లో వాట్సాప్ ఓపెన్ అవుతుంది
     window.open(whatsappURL, '_blank');
 }
